c4bf4bad55869251fd009be8b34efe1a0a3fd0bd
Matt Worzala <mattheworzala@gmail.com>
2020-04-08 00:34:40 -0400

    Initial Commit

---
+++ client/index.js
@@ -0,0 +1,203 @@
+#!/usr/bin/env node
+
+const algosdk = require('algosdk');
+const {getHomeFolder} = require('platform-folders');
+const {join} = require('path');
+const fs = require('fs');
+const rl = require('readline').createInterface({ input: process.stdin, output: process.stdout });
+
+const ALGOD_ADDRESS = 'http://127.0.0.1';
+const ALGOD_PORT = 4001;
+const ALGOD_TOKEN = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+
+const dataFile = join(getHomeFolder(), '.a2f');
+const algod = new algosdk.Algod(ALGOD_TOKEN, ALGOD_ADDRESS, ALGOD_PORT);
+
+const repeat = (func) => new Promise(resolve => {
+    const task = setInterval(async () => {
+        const result = await func();
+        if (result === undefined)
+            return;
+
+        clearInterval(task);
+        resolve(result);
+    }, 4000)
+});
+
+(async () => {
+    if (!fs.existsSync(dataFile)) {
+        console.log("Unable to locate existing user data... Running first time setup");
+        const account = algosdk.generateAccount();
+
+        // Wait for the user to use dispenser.
+        console.log(`\nPlease visit https://bank.testnet.algorand.network/ to fund your account.\nYour address is ${account.addr}.`);
+        await repeat(async () => {
+            const info = await algod.accountInformation(account.addr);
+            if (info.amount !== 0)
+                return true;
+            process.stdout.write('.');
+        });
+        console.log();
+
+        // Create asset
+        process.stdout.write('\nCreating verification asset.')
+        //from, note, total, decimals, defaultFrozen, manager, reserve, freeze,
+        //                             clawback, unitName, assetName, assetURL, assetMetadataHash, suggestedParams
+        const createTxn = algosdk.makeAssetCreateTxnWithSuggestedParams(
+            account.addr,
+            undefined, 1e6, 0, true,
+            account.addr, account.addr,
+            account.addr, account.addr,
+            account.addr.substring(0, 8),
+            'a2f-' + account.addr.substring(0, 8),
+            'https://algorand.com', undefined,
+            await algod.getTransactionParams()
+        );
+        const sCreateTxn = createTxn.signTxn(account.sk);
+        await algod.sendRawTransaction(sCreateTxn);
+
+        const asset = await repeat(async () => {
+            const info = await algod.accountInformation(account.addr);
+            if (info.assets)
+                return parseInt(Object.keys(info.assets)[0]);
+            process.stdout.write('.')
+        });
+        console.log();
+
+        const rawData = {
+            account: algosdk.secretKeyToMnemonic(account.sk),
+            asset,
+            providers: {},
+        };
+        fs.writeFileSync(dataFile, JSON.stringify(rawData));
+
+        console.log('Completed first time setup.\n');
+    }
+
+    const data = JSON.parse(fs.readFileSync(dataFile, 'utf8'));
+    data.account = algosdk.mnemonicToSecretKey(data.account);
+
+    const help = () => {
+        console.log('Algorand 2 Factor Demo');
+        console.log('<description>');
+        console.log('Commands:');
+        console.log('  providers : View a list of authorized providers.');
+        console.log('  add : Authorize a new provider.');
+        console.log('  remove [name] : Remove an authorized provider.');
+        console.log('  verify [name] : Verify a provider.');
+        console.log('  debug (-mnemonic) : View debug information about your Algorand account.');
+    }
+
+    const providers = () => {
+        console.log('Authorized Providers:');
+        data.providers.forEach(provider => console.log('  ' + provider));
+    }
+
+    const add = async () => {
+        process.stdout.write('Waiting for provider information.');
+        let lastCheck = -1;
+        const provider = await repeat(async () => {
+            const params = await algod.getTransactionParams();
+            if (lastCheck === -1)
+                lastCheck = params.lastRound - 1;
+
+            const txns = await algod.transactionByAddress(data.account.addr, lastCheck, params.lastRound);
+            lastCheck = params.lastRound;
+
+            if (!txns.transactions)
+                return;
+            for (let i = 0; i < txns.transactions.length; i++) {
+                const txn = txns.transactions[i];
+                //todo better filtering is likely possible
+                if (txn.type !== 'axfer' || txn.curxfer.rcv !== data.account.addr || txn.curxfer.amt !== 0 || !txn.note)
+                    continue;
+                const note = new TextDecoder().decode(txn.note);
+                return {
+                    address: txn.from,
+                    name: note,
+                };
+            }
+        });
+
+        const auth = await new Promise(
+            resolve => rl.question('Provider \'' + provider.name + '\' has initiated authorization. Accept provider? (y/n)',
+                    ans => resolve(ans.toLowerCase() === 'y')));
+        if (!auth) {
+            console.log('Ignored.')
+            return await add();
+        }
+
+        console.log("Approved.");
+        data.providers[provider.name] = provider.address;
+    }
+
+    const remove = (args) => {
+        if (args.length === 0)
+            console.log('Missing required \'name\' argument!');
+        else if (data.providers[args[0]] === undefined)
+            console.log('Unknown provider ' + args[0] + '');
+        else delete data.providers[args[0]];
+    }
+
+    const verify = async (args) => {
+        if (args.length === 0)
+            console.log('Missing required \'name\' argument!');
+        else if (data.providers[args[0]] === undefined)
+            console.log('Unknown provider ' + args[0] + '');
+        else {
+            const providerAddress = data.providers[args[0]];
+            const verifyTxn = algosdk.makeAssetTransferTxnWithSuggestedParams(
+                data.account.addr, providerAddress,
+                undefined, undefined,
+                1, undefined, data.asset,
+                await algod.getTransactionParams()
+            );
+
+            const sVerifyTxn = verifyTxn.signTxn(data.account.sk);
+            await algod.sendRawTransaction(sVerifyTxn);
+        }
+    }
+
+    const debug = async args => {
+        const params = await algod.getTransactionParams();
+        const info = await algod.accountInformation(data.account.addr);
+        const assetBalance = info.assets[data.asset].amount;
+
+        console.log('Running on Algorand ' + params.genesisID);
+        console.log('Account: ' + data.account.addr);
+        if (args.length !== 0 && args[0] === '-mnemonic')
+            console.log('Mnemonic: ' + algosdk.secretKeyToMnemonic(data.account.sk));
+        console.log('Balance: ' + info.amount + ' MicroAlgos');
+        console.log('Asset Ownership: ' + (assetBalance / 1e4) + '% (' + assetBalance + ')');
+    }
+
+    const args = process.argv.slice(2).map(it => it.toLowerCase());
+    if (args.length === 0)
+        help();
+    else switch (args[0]) {
+        case 'providers':
+        case 'list':
+            providers();
+            break;
+        case 'add':
+            await add();
+            break;
+        case 'remove':
+            remove();
+            break;
+        case 'verify':
+        case 'approve':
+            await verify();
+            break;
+        case 'debug':
+            await debug(args.slice(1));
+            break;
+        default:
+            help();
+    }
+
+    data.account = algosdk.secretKeyToMnemonic(data.account.sk);
+    fs.writeFileSync(dataFile, JSON.stringify(data));
+    rl.close();
+})();
+
